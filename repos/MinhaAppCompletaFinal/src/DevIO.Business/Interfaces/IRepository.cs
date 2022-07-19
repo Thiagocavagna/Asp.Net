08b0b9e61c414c2326efd6f9dc434878c39903d4
Thiagocavagna <97311759+Thiagocavagna@users.noreply.github.com>
2022-07-19 14:44:25 -0300

    Repository / DI

---
+++ repos/MinhaAppCompletaFinal/src/DevIO.Business/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    //interface com os métodos que tem de ser implementados
+    internal interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorID(Guid id);
+        Task<List<TEntity>> ObterTodos();
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid id);
+        //Expressão lambida para buscar entidade
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChances();
+    }
+}
